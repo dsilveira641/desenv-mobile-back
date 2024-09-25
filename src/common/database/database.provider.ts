ab68ce23325e19b355701c7f3d316a64d8a17d75
Daniel Silveira <daniel.silveira@extreme.digital>
2024-09-25 18:28:20 -0300

    Mudança na URL

--- src/common/database/database.provider.ts
+++ src/common/database/database.provider.ts
@@ -4,20 +4,13 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 dotenv.config();
 
-export const dataSourceOptions: DataSourceOptions = {
-    type: process.env.TYPEORM_CONNECTION as any,
-    host: process.env.TYPEORM_HOST,
-    port: process.env.TYPEORM_PORT as any,
-    username: process.env.TYPEORM_USERNAME,
-    password: process.env.TYPEORM_PASSWORD,
-    database: process.env.TYPEORM_DATABASE,
+export const dataSourceOptions: any = {
     url: process.env.TYPEORM_URL,
     entities: [`${__dirname}/entities/{.ts,*.js}`],
     migrations: [`${__dirname}/migrations/{.ts,*.js}`],
     ssl: {
         rejectUnauthorized: false,
     },
-    // NÃO ALTERAR PARA TRUE OS VALORES ENTRE ESSES COMENTÁRIOS
     synchronize: false,
     migrationsRun: false
 } 

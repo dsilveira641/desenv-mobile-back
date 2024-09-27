d87193c42d60d99ecdcd596abfe711c249e15a66
Daniel de Sousa Silveira <dsilveira641@gmail.com>
2024-09-27 14:35:30 -0300

    Ajuste na integração com banco de dados

--- src/common/database/database.provider.ts
+++ src/common/database/database.provider.ts
@@ -5,7 +5,12 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 dotenv.config();
 
 export const dataSourceOptions: any = {
-    url: process.env.TYPEORM_URL,
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
     entities: [`${__dirname}/entities/{.ts,*.js}`],
     migrations: [`${__dirname}/migrations/{.ts,*.js}`],
     ssl: {
